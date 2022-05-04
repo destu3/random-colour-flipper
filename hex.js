6d0c3e85fae31dba7e728f973be4dc22e8f15389
destu3 <obose03@gmail.com>
2022-05-04 09:31:45 +0100

    added important variables and randomHexValue function

--- hex.js
+++ hex.js
@@ -1,1 +1,21 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", () => {
+ 
+});
+
+function randomHexValue(){
+    
+    let hexValue = "#"; 
+    
+    for(i = 0; i < 6; i++){
+        let randomNumber = Math.floor(Math.random() * hex.length);
+        hexValue += hex[randomNumber];
+    }
+    return hexValue;
+};
+
+console.log(randomHexValue());
+
